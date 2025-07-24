81203c6b65d7a69d3ba31e354c194a390bd2c274
b0risfosso <bjfosso@gmail.com>
2025-07-23 19:34:21 -0500

    ugly

---
+++ web/assets/narratives.js
@@ -0,0 +1,8 @@
+// assets/narratives.js
+export const NARRATIVES = [
+    { id: "hindgut",       title: "Hindgut Morphogenesis",   path: "narratives/hindgut.html" },
+    { id: "photosynthesis",title: "Photosynthesis",          path: "narratives/photosynthesis.html" },
+    { id: "missile",       title: "Missile Design",          path: "narratives/missile.html" },
+    // add more here
+  ];
+  
